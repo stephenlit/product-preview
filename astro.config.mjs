e8f0fe0790d8a79ae2897ab41343066e9a628ef2
stephenlit <stevish45@icloud.com>
2024-11-29 15:41:58 -0400

    change path in base in astro.config.mjs file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://stephenlit.github.io',
-    base: '/productPreview',
+    base: 'productPreview',
     integrations: [tailwind()],
 });
